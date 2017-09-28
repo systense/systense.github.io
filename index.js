3c7a4d3c83d91e79a22dd3efc69934d55276b8cc
Maxim <drachyov.maxim@gmail.com>
2017-09-28 21:28:00 +0300

    add arrows

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var current = 0;
     var ul = document.getElementById("project-slider-ul");
     var items = document.querySelectorAll("#project-slider-ul li");
-    // var arrowLeft = document.querySelector(".arrow-left");
-    // var arrowRight = document.querySelector(".arrow-right");
+    var arrowLeft = document.querySelector(".arrow-left");
+    var arrowRight = document.querySelector(".arrow-right");
 
     ul.addEventListener("click", function() {
       if (current == items.length - 1) {
@@ -32,29 +32,29 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
 
-    // arrowLeft.addEventListener("click", function() {
-    //   if (current == 0) {
-    //     items[0].classList.remove("active");
-    //     items[items.length - 1].classList.add("active");
-    //     current = items.length - 1;
-    //   } else {
-    //     items[current].classList.remove("active");
-    //     items[current - 1].classList.add("active");
-    //     current = current - 1;
-    //   }
-    // });
+    arrowLeft.addEventListener("click", function() {
+      if (current == 0) {
+        items[0].classList.remove("active");
+        items[items.length - 1].classList.add("active");
+        current = items.length - 1;
+      } else {
+        items[current].classList.remove("active");
+        items[current - 1].classList.add("active");
+        current = current - 1;
+      }
+    });
 
-    // arrowRight.addEventListener("click", function() {
-    //   if (current == items.length - 1) {
-    //     items[current].classList.remove("active");
-    //     items[0].classList.add("active");
-    //     current = 0;
-    //   } else {
-    //     items[current].classList.remove("active");
-    //     items[current + 1].classList.add("active");
-    //     current = current + 1;
-    //   }
-    // });
+    arrowRight.addEventListener("click", function() {
+      if (current == items.length - 1) {
+        items[current].classList.remove("active");
+        items[0].classList.add("active");
+        current = 0;
+      } else {
+        items[current].classList.remove("active");
+        items[current + 1].classList.add("active");
+        current = current + 1;
+      }
+    });
   }
 });
 
