b47f49fbc161038b6b86d7e502ba35a0f0b2772e
Maxim <drachyov.maxim@gmail.com>
2017-12-16 20:09:33 +0300

    update

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
   var projectSlider = document.getElementById("project-slider");
   var linkDetails = document.getElementById("link-details");
+  var showMenu = document.getElementById("trigger");
+  var secondNav = document.getElementById("secondary-nav");
   var body = document.documentElement;
+
+  if (showMenu) {
+    showMenu.addEventListener("click", function() {
+      if (!showMenu.classList.contains("current")) showMenu.classList.add("current");
+      if (secondNav.classList.contains("hidden")) secondNav.classList.remove("hidden");
+    });
+  }
 
   if (linkDetails) {
     linkDetails.addEventListener("click", function() {
       var wrapper = document.querySelector(".details .wrapper");
       if (!wrapper.classList.contains("active")) wrapper.classList.add("active");
-      // setTimeout(function() {
-        scrollTo(body, projectSlider.offsetHeight, 300);
-      // }, 300);
+      scrollTo(body, projectSlider.offsetHeight, 300);
     });
   }
 
