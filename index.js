18dced1ce8445d05859ea2633644fa822eddea3e
Maxim <drachyov.maxim@gmail.com>
2017-12-18 15:06:45 +0300

    update

--- index.js
+++ index.js
@@ -20,51 +20,52 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
-  console.log(projectSlider);
+  if (projectSlider) {
+    projectSlider.forEach(function() {
+      console.log(this);
+    //   var current = 0;
+    //   var ul = document.getElementById("project-slider-ul");
+    //   var items = document.querySelectorAll("#project-slider-ul li");
+    //   var arrowLeft = document.querySelector(".arrow-left");
+    //   var arrowRight = document.querySelector(".arrow-right");
 
-  // if (projectSlider) {
-  //   var current = 0;
-  //   var ul = document.getElementById("project-slider-ul");
-  //   var items = document.querySelectorAll("#project-slider-ul li");
-  //   var arrowLeft = document.querySelector(".arrow-left");
-  //   var arrowRight = document.querySelector(".arrow-right");
+    //   ul.addEventListener("click", function() {
+    //     if (current == items.length - 1) {
+    //       items[current].classList.remove("active");
+    //       items[0].classList.add("active");
+    //       current = 0;
+    //     } else {
+    //       items[current].classList.remove("active");
+    //       items[current + 1].classList.add("active");
+    //       current = current + 1;
+    //     }
+    //   });
 
-  //   ul.addEventListener("click", function() {
-  //     if (current == items.length - 1) {
-  //       items[current].classList.remove("active");
-  //       items[0].classList.add("active");
-  //       current = 0;
-  //     } else {
-  //       items[current].classList.remove("active");
-  //       items[current + 1].classList.add("active");
-  //       current = current + 1;
-  //     }
-  //   });
+    //   arrowLeft.addEventListener("click", function() {
+    //     if (current == 0) {
+    //       items[0].classList.remove("active");
+    //       items[items.length - 1].classList.add("active");
+    //       current = items.length - 1;
+    //     } else {
+    //       items[current].classList.remove("active");
+    //       items[current - 1].classList.add("active");
+    //       current = current - 1;
+    //     }
+    //   });
 
-  //   arrowLeft.addEventListener("click", function() {
-  //     if (current == 0) {
-  //       items[0].classList.remove("active");
-  //       items[items.length - 1].classList.add("active");
-  //       current = items.length - 1;
-  //     } else {
-  //       items[current].classList.remove("active");
-  //       items[current - 1].classList.add("active");
-  //       current = current - 1;
-  //     }
-  //   });
-
-  //   arrowRight.addEventListener("click", function() {
-  //     if (current == items.length - 1) {
-  //       items[current].classList.remove("active");
-  //       items[0].classList.add("active");
-  //       current = 0;
-  //     } else {
-  //       items[current].classList.remove("active");
-  //       items[current + 1].classList.add("active");
-  //       current = current + 1;
-  //     }
-  //   });
-  // }
+    //   arrowRight.addEventListener("click", function() {
+    //     if (current == items.length - 1) {
+    //       items[current].classList.remove("active");
+    //       items[0].classList.add("active");
+    //       current = 0;
+    //     } else {
+    //       items[current].classList.remove("active");
+    //       items[current + 1].classList.add("active");
+    //       current = current + 1;
+    //     }
+    //   });
+    });
+  }
 });
 
 function scrollTo(element, to, duration) {
